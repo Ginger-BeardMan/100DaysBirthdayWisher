311cd4fde745d26bbaf6fd87093722bd04c69a25
Stephen Karney <112995644+Ginger-BeardMan@users.noreply.github.com>
2023-11-17 07:03:00 -0500

    Update main.py
    added official birthday letter functionality

--- main.py
+++ main.py
@@ -12,35 +12,29 @@ today = str(now.date())
 # Birthday Information
 data_file = pd.read_csv('birthdays.csv')
 birthday_dict = data_file.to_dict(orient='records')
-celebrator = random.randint(0, len(birthday_dict)-1)
-birthday = f"{birthday_dict[celebrator]['year']}-{birthday_dict[celebrator]['month']}-{birthday_dict[celebrator]['day']}"
 
-# Contact Information
+# Sender Contact Information
 my_email = 'chungus3535@gmail.com'
 password = 'abcd1357)('
 
-
 # ------------------------- Correspondence ------------------------------
 
-# Message
-with open()
+letter_templates = ['letter_1.txt', 'letter_2.txt', 'letter_3.txt']
 
-# Email
-with smtplib.SMTP('smtp.gmail.com') as connection:
-    connection.starttls()
-    connection.login(user=my_email, password=password)
-    connection.sendmail(from_addr=my_email, to_addrs=birthday_dict[celebrator]['email'], msg=)
+for person in birthday_dict:
+    birthday = f"{person['year']}-{person['month']}-{person['day']}"
 
-# ------------------- Extra Hard Starting Project ----------------------
+    if birthday == today:
 
-# 1. Update the birthdays.csv
+        # Message
+        with open(f'letter_templates/{random.choice(letter_templates)}', 'r') as b_day_letter:
+            letter = b_day_letter.read()
+            official_letter = letter.replace('[NAME]', person['name'])
+            print(official_letter)
 
-# 2. Check if today matches a birthday in the birthdays.csv
+            # Email
+            with smtplib.SMTP('smtp.gmail.com') as connection:
+                connection.starttls()
+                connection.login(user=my_email, password=password)
+                connection.sendmail(from_addr=my_email, to_addrs=person['email'], msg=official_letter)
 
-if birthday == today:
-    print('Happy Birthday!')
-
-# 3. If step 2 is true,
-# pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
-
-# 4. Send the letter generated in step 3 to that person's email address.
